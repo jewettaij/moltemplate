25cb951fe00c0c744cc09299f82b7a343f0583e8
Andrew Jewett <jewett.aij@gmail.com>
2019-12-10 15:03:51 -0800

    cleaned up and documented the DNA example ("3bp=2beads") somewhat.

--- examples/coarse_grained/DNA_models/dsDNA/2strands/dsDNA_3bp=2beads_2019-10-19/STEP_1_generate_initial_path.sh
+++ examples/coarse_grained/DNA_models/dsDNA/2strands/dsDNA_3bp=2beads_2019-10-19/STEP_1_generate_initial_path.sh
@@ -3,16 +3,13 @@
 
 DEST_FILE="moltemplate_files/init_crds_polymer_backbone.raw"
 rm -f "$DEST_FILE"
-NPOINTS=32
-#LMONOMER=1.0108   #<--relaxed distance between monomers along the polymer axis
-LMONOMER=1.00       #<--I shortened it a little to start with a smaller circle
-                    #   (Be sure to run minimization beforehand to relax 
-                    #    the bonds to their rest lengths.)
+NMONOMERS=32
+LMONOMER=0.98293   #<--relaxed distance between monomers along the polymer axis
 PI=3.141592653589793
-RCIRCLE=`echo "$LMONOMER*$NPOINTS/(2*$PI)" | bc -l`
-for ((i=0; i<$NPOINTS; i++)); do 
-    XCOORD=`echo "$RCIRCLE*c(($i+0.5)*2*$PI/$NPOINTS)" | bc -l`
-    YCOORD=`echo "$RCIRCLE*s(($i+0.5)*2*$PI/$NPOINTS)" | bc -l`
+RCIRCLE=`echo "$LMONOMER*$NMONOMERS/(2*$PI)" | bc -l`
+for ((i=0; i<$NMONOMERS; i++)); do 
+    XCOORD=`echo "$RCIRCLE*c(($i+0.5)*2*$PI/$NMONOMERS)" | bc -l`
+    YCOORD=`echo "$RCIRCLE*s(($i+0.5)*2*$PI/$NMONOMERS)" | bc -l`
     ZCOORD=0.0
     echo "$XCOORD $YCOORD $ZCOORD" >> "$DEST_FILE"
 done
--- examples/coarse_grained/DNA_models/dsDNA/2strands/dsDNA_3bp=2beads_2019-10-19/measure_torsional_persistence_length/STEP_1_generate_initial_path.sh
+++ examples/coarse_grained/DNA_models/dsDNA/2strands/dsDNA_3bp=2beads_2019-10-19/measure_torsional_persistence_length/STEP_1_generate_initial_path.sh
@@ -2,13 +2,15 @@
 
 DEST_FILE="moltemplate_files/init_crds_polymer_backbone.raw"
 rm -f "$DEST_FILE"
-NPOINTS=32
-LMONOMER=1.0108   #<--relaxed distance between monomers along the polymer axis
+NMONOMERS=32
+LMONOMER=1.0   #<-relaxed distance between monomers along the polymer axis (nm).
+               #  (This is just a guess. We will have to run some simulations at
+               #   the desired temperature to learn what the actual length is.)
 
-for ((i=0; i<$NPOINTS; i++)); do 
+for ((i=0; i<$NMONOMERS; i++)); do 
     XCOORD=0.0
     YCOORD=0.0
-    ZCOORD=`echo "$LMONOMER*($i+0.5-0.5*$NPOINTS)" | bc -l`
+    ZCOORD=`echo "$LMONOMER*($i+0.5-0.5*$NMONOMERS)" | bc -l`
     echo "$XCOORD $YCOORD $ZCOORD" >> "$DEST_FILE"
 done
 
