3db3c7277a348fb6551144b06c9967a2fed75606
Andrew Jewett <jewett.aij@gmail.com>
2021-04-08 19:45:53 -0700

    changed version info

--- moltemplate/scripts/moltemplate.sh
+++ moltemplate/scripts/moltemplate.sh
@@ -6,8 +6,8 @@
 # Copyright (c) 2013
 
 G_PROGRAM_NAME="moltemplate.sh"
-G_VERSION="2.19.7"
-G_DATE="2021-3-10"
+G_VERSION="2.19.8"
+G_DATE="2021-4-08"
 
 echo "${G_PROGRAM_NAME} v${G_VERSION} ${G_DATE}" >&2
 echo "" >&2
--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
 
   url='https://github.com/jewettaij/moltemplate',
 
-  download_url='https://github.com/jewettaij/moltemplate/archive/v2.19.7.zip',
+  download_url='https://github.com/jewettaij/moltemplate/archive/v2.19.8.zip',
 
-  version='2.19.7',
+  version='2.19.8',
 
   keywords=['simulation', 'LAMMPS', 'molecule editor', 'molecule builder',
             'ESPResSo'],
