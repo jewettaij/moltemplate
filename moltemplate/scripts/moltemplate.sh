1126dc939db73c829ffcd86dc4f4e01110583ab5
Andrew Jewett <jewett.aij@gmail.com>
2022-01-19 11:10:12 -0800

    updated version info

--- moltemplate/scripts/moltemplate.sh
+++ moltemplate/scripts/moltemplate.sh
@@ -6,8 +6,8 @@
 # Copyright (c) 2013
 
 G_PROGRAM_NAME="moltemplate.sh"
-G_VERSION="2.20.0"
-G_DATE="2022-1-11"
+G_VERSION="2.20.1"
+G_DATE="2022-1-19"
 
 echo "${G_PROGRAM_NAME} v${G_VERSION} ${G_DATE}" >&2
 echo "" >&2
