530df8d05996366dcceb53cebc1aa98a312f6aeb
Andrew Jewett <jewett.aij@gmail.com>
2022-04-06 11:05:03 -0700

    updated the molc unit test (it was crashing earlier)

--- tests/test_molc.sh
+++ tests/test_molc.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 test_oplsaa() {
-  cd test_molc_files/
-    moltemplate.sh -atomstyle "hybrid molecular ellipsoid" -molc system.lt
-    assertTrue "system.data file not created" "[ -s system.data ]"
-    assertTrue "system.in.settings file not created" "[ -s system.in.settings ]"
+  cd tests/
+    cd test_molc_files/
+      moltemplate.sh -atomstyle "hybrid molecular ellipsoid" -molc system.lt
+      assertTrue "system.data file not created" "[ -s system.data ]"
+      assertTrue "system.in.settings file not created" "[ -s system.in.settings ]"
+    cd ../
   cd ../
 }
 
