c76cb98a817c1c7fc5b0f8b926b72fa032709dcd
Andrew Jewett <jewett.aij@gmail.com>
2022-07-06 10:20:31 -0700

    updated version info

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
 
   url='https://github.com/jewettaij/moltemplate',
 
-  download_url='https://github.com/jewettaij/moltemplate/archive/v2.20.6.zip',
+  download_url='https://github.com/jewettaij/moltemplate/archive/v2.20.7.zip',
 
-  version='2.20.6',
+  version='2.20.7',
 
   keywords=['simulation', 'LAMMPS', 'molecule editor', 'molecule builder',
             'ESPResSo'],
