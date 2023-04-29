2b5c9a7626ae542dfa72455ee108fc1fcd149e68
Andrew Jewett <jewett.aij@gmail.com>
2023-04-29 11:21:44 -0700

    atom creation is now optional

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
 
   url='https://github.com/jewettaij/moltemplate',
 
-  download_url='https://github.com/jewettaij/moltemplate/archive/v2.20.19.zip',
+  download_url='https://github.com/jewettaij/moltemplate/archive/v2.20.20.zip',
 
-  version='2.20.19',
+  version='2.20.20',
 
   keywords=['simulation', 'LAMMPS', 'molecule editor', 'molecule builder',
             'ESPResSo'],
